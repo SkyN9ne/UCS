73347cc6287f41be97e7812ca9e4e4aa451f8eb4
smartclash <smartclashcoc@gmail.com>
2016-10-04 16:13:40 +0530

    Added logger with features

---
+++ Ultrapowa Clash Server/Core/loggerTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCS.Core
+{
+   public enum Types
+   {
+        INFO,
+        DEBUG,
+        WARNING,
+        ERROR
+   }
+}
